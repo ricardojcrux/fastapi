f4737f48e74ebd5451d36d2986b89df724b33011
Ricardo Zapata <ricardozapatacruz@gmail.com>
2023-04-18 23:21:15 -0500

    cambios en el gitignore y avance en sqlalchemy

--- course/config/database.py
+++ course/config/database.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine    #Importamos dependencia del motor de la 
 from sqlalchemy.orm.session import sessionmaker         #
 from sqlalchemy.ext.declarative import declarative_base
 
-sqlite_db = 'database.sqlite'           #Nombre de la DB
+sqlite_db = '../database.sqlite'           #Nombre de la DB
 db_dir = os.path.dirname(os.path.realpath(__file__))    #Trayendo la dirección del archivo
 
 db_url = f'sqlite:///{os.path.join(db_dir,sqlite_db)}'  #Conectando la dirección de la DB con su nombre
 
-motor = create_engine(db_url, echo=True)    #Creación del motor de la DB
-sesion = sessionmaker(bind=motor)           #Sesion enlazada con el motor de la DB
-base = declarative_base()                   #Funcion que nos sirve para manejar tablas de la DB
+Motor = create_engine(db_url, echo=True)    #Creación del motor de la DB
+Sesion = sessionmaker(bind=Motor)           #Sesion enlazada con el motor de la DB
+Base = declarative_base()                   #Funcion que nos sirve para manejar tablas de la DB
--- course/main.py
+++ course/main.py
@@ -6,19 +6,20 @@ from fastapi import Path, Query, Request,Depends
 from fastapi.responses import HTMLResponse  #Modulo que nos deja hacer codigo HTML
 from fastapi.responses import JSONResponse  #Modulo que devuelve en formato JSON
 from fastapi.security import HTTPBearer
-
 from pydantic import BaseModel  #Modulo que con una clase crea un modelo con las variables
 from pydantic import Field      #Rangos de campos en clases
-
 from typing import Optional     #Para que un request sea opcional
 from typing import List         #Lista
-
 from jwt_manager import create_token #Crear token jwt
 from jwt_manager import validate_token
+from config.database import Motor, Sesion, Base
+from models.movie import Movie as MovieModel
 
 app = FastAPI()                     #Llamamos una API como app
 app.title = "Mi primer FastAPI"     #Titulo del API
 app.version = "29.07.22"            #Ponemos version de manera manual
+
+Base.metadata.create_all(bind=Motor)
 
 class JWTBearer(HTTPBearer):
     async def __call__(self, request: Request):
@@ -34,7 +35,6 @@ class User(BaseModel):
 
 #Creamos una clase como modelo de atributos de una pelicula
 class Movie(BaseModel):
-    id: Optional[int] = None
     title: str = Field(min_length=5,max_length=15)
     overview: str
     year: int = Field(le=2024)
@@ -45,7 +45,6 @@ class Movie(BaseModel):
     class Config:
         schema_extra={
             'example': {
-                'id':0,
                 'title':'Movie',
                 'overview':'Overview',
                 'year':0,
@@ -80,11 +79,13 @@ movies = [
 def message():
     return HTMLResponse('<h1 style=color:teal>Hello world</h1>')
 
-@app.post('/login', tags=['auth'])
+@app.post('/login', tags=['Authentication'])
 def login(user: User):
     if user.email == 'admin@gmail.com' and user.password == 'admin':
         token: str = create_token(user.dict())
-    return JSONResponse(status_code=200, content=token)
+        return JSONResponse(status_code=200, content=token)
+    else:
+        return JSONResponse(status_code=404,content={'message':'El usuario es invalido'})
 
 #Get que retorna la lista de diccionarios
 @app.get('/movies',tags=['Movies'], response_model=List[Movie], status_code=200,dependencies=[Depends(JWTBearer())])
@@ -97,7 +98,7 @@ def get_id(id: int = Path(ge=1,le=2000)) -> Movie:
     for item in movies:
         if item['id'] == id:
             return JSONResponse(content=item)
-    return JSONResponse(status_code=404, content=[])
+    return JSONResponse(status_code=404, content={'message':'La pelicula no existe'})
 
 #Get que retorna una lista de diccionarios con esa categoria
 @app.get('/movies/',tags=['Movies'],response_model=List[Movie],status_code=200)
@@ -109,8 +110,11 @@ def get_movies_by_category(category:str = Query(min_length=5,max_length=15)) -> 
 #Post que añade un diccionario mas en la lista
 @app.post('/movies',tags=['Movies'], response_model=dict,status_code=201)
 def create_movies(movie: Movie) -> dict:
-    movies.append(dict(movie))
-    return JSONResponse(status_code=201, content={'message:','Se ha registrado la película'})
+    db = Sesion()
+    new_movie = MovieModel(**movie.dict())
+    db.add(new_movie)
+    db.commit()
+    return JSONResponse(status_code=201, content={'message':'Se ha registrado la película'}) 
 
 @app.put('/movies/{id}', tags=['Movies'], response_model=dict,status_code=200)
 def update_movie(id:int, movie:Movie) -> dict:
@@ -121,11 +125,13 @@ def update_movie(id:int, movie:Movie) -> dict:
             item['year'] = movie.year
             item['rating'] = movie.rating
             item['category'] = movie.category
-            return JSONResponse(status_code=200,content={'message:','Se ha modificado la película'})
+            return JSONResponse(status_code=200,content={'message':'Se ha modificado la película'})
         
 @app.delete('/movies/{id}', tags=['Movies'], response_model=dict,status_code=200)
 def delete_movie(id:int) -> dict:
     for item in movies:
         if item["id"] == id:
             movies.remove(item)
-            return JSONResponse(status_code=200, content={'message:','Se ha eliminado la película'})
+            return JSONResponse(status_code=200, content={'message':'Se ha eliminado la película'})
+        else:
+            return JSONResponse(status_code=404, content={'message':'Pelicula no existente'})
