e62008cce7f0fa3b620baffd26bf6be13794e700
Ricardo Zapata <ricardozapatacruz@gmail.com>
2023-05-08 11:24:30 -0500

    cambios ya

--- course/main.py
+++ course/main.py
@@ -26,7 +26,3 @@ def message():
     <h2><a href=/docs>Documentación de la API</a></h2>
     </center></body>
     '''
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port = int(os.environ.get("PORT",8000)))
-    
